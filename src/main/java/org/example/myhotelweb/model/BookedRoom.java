046f8dd9cd354b79f4aa16935830cf45c9c88b98
Hoon <hoon0661@gmail.com>
2024-04-01 13:20:59 -0400

    added initial domains - Room & BookedRoom

--- src/main/java/org/example/myhotelweb/model/BookedRoom.java
+++ src/main/java/org/example/myhotelweb/model/BookedRoom.java
@@ -1,4 +1,65 @@
 package org.example.myhotelweb.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_Fullname")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_Guests")
+    private int totalNumberOfGuests;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_Id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuests() {
+        this.totalNumberOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuests();
+    }
+
 }
+
