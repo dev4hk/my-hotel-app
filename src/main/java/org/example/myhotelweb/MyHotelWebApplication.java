3076e7d08cd662d9250cb2636a851d6eff1866fd
Hoon <hoon0661@gmail.com>
2024-04-01 10:33:27 -0400

    initial commit

---
+++ src/main/java/org/example/myhotelweb/MyHotelWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.myhotelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHotelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHotelWebApplication.class, args);
+	}
+
+}
