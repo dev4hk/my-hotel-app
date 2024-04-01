12777fe62c4eae273668e7cf116fca6e6af97969
Hoon <hoon0661@gmail.com>
2024-04-01 18:20:51 -0400

    added initial service implementation - booking service & room service

--- src/main/java/org/example/myhotelweb/service/BookingServiceImpl.java
+++ src/main/java/org/example/myhotelweb/service/BookingServiceImpl.java
@@ -1,39 +1,70 @@
 package org.example.myhotelweb.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.myhotelweb.exception.InvalidBookingRequestException;
+import org.example.myhotelweb.exception.ResourceNotFoundException;
+import org.example.myhotelweb.model.Booking;
 import org.example.myhotelweb.model.Room;
+import org.example.myhotelweb.repository.BookingRepository;
 import org.example.myhotelweb.repository.RoomRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.sql.rowset.serial.SerialBlob;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.sql.Blob;
-import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BookingServiceImpl implements IRoomService {
+public class BookingServiceImpl implements IBookingService {
 
-    private final RoomRepository roomRepository;
+    private final BookingRepository bookingRepository;
+    private final IRoomService roomService;
+
 
     @Override
-    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws SQLException, IOException {
-        Room room = new Room();
-        room.setRoomType(roomType);
-        room.setRoomPrice(roomPrice);
-        if(!file.isEmpty()) {
-            byte[] photoBytes = file.getBytes();
-            Blob photoBlob = new SerialBlob(photoBytes);
-            room.setPhoto(photoBlob);
-        }
-        return roomRepository.save(room);
+    public List<Booking> getAllBookingsByRoomId(Long roomId) {
+        return bookingRepository.findByRoomId(roomId);
     }
 
     @Override
-    public List<String> getAllRoomTypes() {
-        return roomRepository.findDistinctRoomTypes();
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    @Override
+    public String saveBooking(Long roomId, Booking bookingRequest) {
+        if(bookingRequest.getCheckOutDate().isBefore(bookingRequest.getCheckInDate())) {
+            throw new InvalidBookingRequestException("Check-in date must come before check-out date");
+        }
+        Room room = roomService.getRoomById(roomId).get();
+        List<Booking> currentBookings = room.getBookings();
+        boolean isRoomAvailable = isRoomAvailable(bookingRequest, currentBookings);
+        if(isRoomAvailable) {
+            room.addBooking(bookingRequest);
+            bookingRepository.save(bookingRequest);
+        }
+        else {
+            throw new InvalidBookingRequestException("Room not available");
+        }
+        return bookingRequest.getBookingConfirmationCode();
+    }
+
+    private boolean isRoomAvailable(Booking bookingRequest, List<Booking> currentBookings) {
+        return currentBookings.stream()
+                .noneMatch(existingBooking ->
+                        bookingRequest.getCheckInDate().equals(existingBooking.getCheckInDate())
+                                || bookingRequest.getCheckOutDate().isBefore(existingBooking.getCheckOutDate())
+                                || (bookingRequest.getCheckInDate().isAfter(existingBooking.getCheckInDate()) && bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate()) && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate()) && bookingRequest.getCheckOutDate().isAfter(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate()) && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckInDate()))
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate()) && bookingRequest.getCheckOutDate().equals(bookingRequest.getCheckInDate()))
+                );
+
+    }
+
+    @Override
+    public Booking findByBookingConfirmationCode(String confirmationCode) {
+        return bookingRepository.findByBookingConfirmationCode(confirmationCode)
+                .orElseThrow(() -> new ResourceNotFoundException("No booking found with the booking code :" + confirmationCode));
     }
 }
--- src/main/java/org/example/myhotelweb/service/RoomServiceImpl.java
+++ src/main/java/org/example/myhotelweb/service/RoomServiceImpl.java
@@ -1,4 +1,64 @@
 package org.example.myhotelweb.service;
 
-public class RoomServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.myhotelweb.exception.ResourceNotFoundException;
+import org.example.myhotelweb.model.Room;
+import org.example.myhotelweb.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class RoomServiceImpl implements IRoomService{
+
+    private final RoomRepository roomRepository;
+
+    @Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws SQLException, IOException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if(!file.isEmpty()) {
+            byte[] photoBytes = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return roomRepository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomPhotoByRoomId(Long roomId) throws SQLException {
+        Optional<Room> room = roomRepository.findById(roomId);
+        if(room.isEmpty()) {
+            throw new ResourceNotFoundException("Room not found.");
+        }
+        Blob photoBlob = room.get().getPhoto();
+        if(photoBlob != null) {
+            return photoBlob.getBytes(1, (int) photoBlob.length());
+        }
+        return null;
+    }
+
+    @Override
+    public Optional<Room> getRoomById(Long roomId) {
+        return roomRepository.findById(roomId);
+    }
 }
