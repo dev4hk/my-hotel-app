b19def034909295881ea359b1d7d57ea40464083
Hoon <hoon0661@gmail.com>
2024-04-01 18:20:15 -0400

    added interface - room service & booking service

--- src/main/java/org/example/myhotelweb/service/IBookingService.java
+++ src/main/java/org/example/myhotelweb/service/IBookingService.java
@@ -1,4 +1,13 @@
 package org.example.myhotelweb.service;
 
+import org.example.myhotelweb.model.Booking;
+
+import java.util.List;
+
 public interface IBookingService {
+    List<Booking> getAllBookingsByRoomId(Long roomId);
+    List<Booking> getAllBookings();
+    String saveBooking(Long roomId, Booking booking);
+    Booking findByBookingConfirmationCode(String confirmationCode);
+
 }
