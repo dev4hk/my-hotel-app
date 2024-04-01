bd242627a27b77d6b66797d9c0c5fea9fbe96b38
Hoon <hoon0661@gmail.com>
2024-04-01 18:19:39 -0400

    added booking repository

--- src/main/java/org/example/myhotelweb/repository/BookingRepository.java
+++ src/main/java/org/example/myhotelweb/repository/BookingRepository.java
@@ -1,4 +1,13 @@
 package org.example.myhotelweb.repository;
 
-public interface Booking {
+import org.example.myhotelweb.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByRoomId(Long roomId);
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
 }
