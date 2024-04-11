fb3518261ec0ce2904ffc19e8f826a311e70633b
Hoon <hoon0661@gmail.com>
2024-04-10 21:58:39 -0400

    added method to find available rooms by dates and room type

--- src/main/java/org/example/myhotelweb/repository/RoomRepository.java
+++ src/main/java/org/example/myhotelweb/repository/RoomRepository.java
@@ -4,10 +4,19 @@ import org.example.myhotelweb.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     @Query("SELECT DISTINCT r.roomType FROM Room r")
     List<String> findDistinctRoomTypes();
+
+    @Query(" SELECT r FROM Room r " +
+            " WHERE r.roomType LIKE %:roomType% " +
+            " AND r.id NOT IN (" +
+            " SELECT b.room.id FROM Booking b " +
+            " WHERE ((b.checkInDate <= :checkOutDate) AND (b.checkOutDate >= :checkInDate))" +
+            ")")
+    List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
