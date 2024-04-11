5cb66b8bc807952ea586e5e623fa2a5325af66dc
Hoon <hoon0661@gmail.com>
2024-04-11 17:53:01 -0400

    added methods to allow all CRUD methods

--- src/main/java/org/example/myhotelweb/config/CorsConfig.java
+++ src/main/java/org/example/myhotelweb/config/CorsConfig.java
@@ -4,17 +4,16 @@ package org.example.myhotelweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer configure() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry reg) {
-                reg.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
     }
 }
