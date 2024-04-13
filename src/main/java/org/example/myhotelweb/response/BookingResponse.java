9f6dc9700d46bb788bdcd31bce9bff8853ec9bd4
Hoon <hoon0661@gmail.com>
2024-04-13 18:12:30 -0400

    added @JsonFormat on LocalDate fields to retrieve ISO format of time

--- src/main/java/org/example/myhotelweb/response/BookingResponse.java
+++ src/main/java/org/example/myhotelweb/response/BookingResponse.java
@@ -1,10 +1,12 @@
 package org.example.myhotelweb.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.myhotelweb.model.Room;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -15,8 +17,10 @@ public class BookingResponse {
 
     private Long bookingId;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate checkInDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate checkOutDate;
 
     private String guestFullName;
