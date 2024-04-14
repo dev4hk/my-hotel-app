2ceabb455a13072e6c7cf20db8f44b2d7aab28c9
Hoon <hoon0661@gmail.com>
2024-04-14 16:57:32 -0400

    added component for room search

--- src/frontend/src/components/home/Home.jsx
+++ src/frontend/src/components/home/Home.jsx
@@ -3,14 +3,14 @@ import MainHeader from "../layout/MainHeader";
 import HotelService from "../common/HotelService";
 import Parallax from "../common/Parallax";
 import RoomCarousel from "../common/RoomCarousel";
-// import RoomSearch from "../common/RoomSearch";
+import RoomSearch from "../common/RoomSearch";
 
 const Home = () => {
   return (
     <section>
       <MainHeader />
       <div className="container">
-        {/* <RoomSearch /> */}
+        <RoomSearch />
         <Parallax />
         <RoomCarousel />
         <HotelService />
