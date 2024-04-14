ce3f70234ffad66b35b3dd98bac867b25b005541
Hoon <hoon0661@gmail.com>
2024-04-14 18:07:51 -0400

    added user service

---
+++ src/main/java/org/example/myhotelweb/service/UserService.java
@@ -0,0 +1,55 @@
+package org.example.myhotelweb.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.myhotelweb.exception.UserAlreadyExistsException;
+import org.example.myhotelweb.model.Role;
+import org.example.myhotelweb.model.User;
+import org.example.myhotelweb.repository.RoleRepository;
+import org.example.myhotelweb.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new UserAlreadyExistsException(user.getEmail() + " already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        user.setRoles(Collections.singletonList(userRole));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(String email) {
+        User theUser = getUser(email);
+        if (theUser != null){
+            userRepository.deleteByEmail(email);
+        }
+
+    }
+
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
