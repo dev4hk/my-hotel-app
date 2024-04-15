2f7e38ad8c441e9c0ad2505c4737f4d2fb44a336
Hoon <hoon0661@gmail.com>
2024-04-15 13:40:17 -0400

    added role service

---
+++ src/main/java/org/example/myhotelweb/service/IRoleService.java
@@ -0,0 +1,18 @@
+package org.example.myhotelweb.service;
+
+import org.example.myhotelweb.model.Role;
+import org.example.myhotelweb.model.User;
+
+import java.util.List;
+
+public interface IRoleService {
+    List<Role> getRoles();
+    Role createRole(Role theRole);
+
+    void deleteRole(Long id);
+    Role findByName(String name);
+
+    User removeUserFromRole(Long userId, Long roleId);
+    User assignRoleToUser(Long userId, Long roleId);
+    Role removeAllUsersFromRole(Long roleId);
+}
